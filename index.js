4ff222195d463682f346289f5bc3cb5f9752aa27
shahid iqbal <shahidql@yahoo.ca>
2019-06-03 16:56:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const http = require('http');
+const path = require('path');
 
 const port=process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  //res.end('<h1>Hello World</h1>');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(port,() => {
